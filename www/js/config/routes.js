794ac2a05ee8b913ebcd7c4317814ade2922ae4c
Krystal Mejia <krystalmejia24@gmail.com>
2016-10-01 20:05:50 -0400

    cleanup, moved routes and services to seperate files

---
+++ www/js/config/routes.js
@@ -0,0 +1,30 @@
+angular.module('mTrail').config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  //States go here
+  .state('menu', {
+    url: '/',
+    templateUrl: 'templates/menu.html',
+    controller: 'MenuController'
+  })
+  .state('list', {
+    url: '/list',
+    templateUrl: 'templates/list.html',
+    controller: 'MenuController',
+  })
+  .state('county', {
+    url: '/county',
+    templateUrl: 'templates/county.html',
+    controller: 'CountyController',
+  })
+  .state('boundary', {
+    url: '/boundary',
+    templateUrl: 'templates/boundary.html',
+    controller: 'BoundaryController',
+    params: {
+          boundaryId: Number
+    },
+  });
+
+  $urlRouterProvider.otherwise("/");
+});
